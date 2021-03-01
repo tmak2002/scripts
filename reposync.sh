75c9e372444e50742b997d4f7606edb5ad0d8039
tmak2002 <tmak@disroot.org>
2021-03-01 11:17:59 +0100

    update repo sync script

--- reposync.sh
+++ reposync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 START=$(date +%s)
-repo sync -j8
+repo sync
 END=$(date +%s)
 SYNCTIME="Sync time: $(echo $((${END}-${START})) | awk '{print int($1/60)" minutes and "int($1%60)" seconds"}')"
 echo $SYNCTIME > /home/torben/synctime.txt

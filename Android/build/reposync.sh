21d79f241941662e5ccbcf84c4497d6164a772b2
tmak2002 <tmak@disroot.org>
2021-03-01 13:01:06 +0100

    update repo sync script

--- Android/build/reposync.sh
+++ Android/build/reposync.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 START=$(date +%s)
+echo $1 >> repo_url
+if grep -wFo https://android.googlesource.com/platform/manifest/ repo_url*; then
+    repo init -u $1 -b $2
+# TODO: Add more Options
+else
+    echo """
+    usage: repo sync url branch
+    """
+fi
+rm repo_url
 repo sync
 END=$(date +%s)
 SYNCTIME="Sync time: $(echo $((${END}-${START})) | awk '{print int($1/60)" minutes and "int($1%60)" seconds"}')"
